91980a8d45b5c328be3ce39b4f8c5ae3d74611d3
Gabe Jensen <thegabejensen@gmail.com>
2023-05-01 21:55:40 -0700

    Updated gabe test

--- __tests__/gabe.test.tsx
+++ __tests__/gabe.test.tsx
@@ -7,38 +7,30 @@ import "whatwg-fetch";
 // your testing framework configuration rather than importing them in every file.
 import "@testing-library/jest-dom";
 import * as React from "react";
-// import API mocking utilities from Mock Service Worker.
-import { rest } from "msw";
-import { setupServer } from "msw/node";
-// import testing utilities
 import { render, fireEvent, screen } from "@testing-library/react";
 import { server } from "../mocks/server";
 import GabeDashboard from "../components/GabeDashboard";
 
-function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+// function delay(ms: number) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
 
-// const server = setupServer(
-//   rest.post("/api/gabe", (req, res, ctx) => {
-//     return res(
-//       ctx.json({
-//         first_name: "Gabe",
-//         last_name: "Jensen",
-//       })
-//     );
-//   })
-// );
-
-test("Does MSW and real life requests", async () => {
+beforeAll(() => {
   server.listen();
+});
 
+test("MSW works", async () => {
   render(<GabeDashboard />);
   fireEvent.click(screen.getByTestId(/gabe-btn/i));
 
   const content = await screen.findByTestId("gabe-text");
   expect(content).toHaveTextContent(/Jones/i);
+});
 
+afterEach(() => {
   server.resetHandlers();
+});
+
+afterAll(() => {
   server.close();
 });
